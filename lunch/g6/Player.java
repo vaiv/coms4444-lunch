6d5fdb637f8aef2336503a884235d17bc99934f4
Patrick Kwok <patlkwok@outlook.com>
2019-11-15 19:58:43 -0500

    Update some helper functions

--- lunch/g6/Player.java
+++ lunch/g6/Player.java
@@ -30,7 +30,7 @@ public class Player implements lunch.sim.Player {
         turn = 0;
     }
     
-    public void init(ArrayList<Family> members, Integer id, int f, ArrayList<Animal> animals, Integer m, Integer g, double t, Integer s) {
+    public void init(ArrayList<Family> members, int id, int f, ArrayList<Animal> animals, int m, int g, double t, int s) {
         this.id = id;
         avatars = "flintstone";
         random = new Random(s);
@@ -47,7 +47,7 @@ public class Player implements lunch.sim.Player {
         }
         
         if (turn < 100) {
-            boolean found_valid_move= false;
+            boolean found_valid_move = false;
             Point next_move = new Point(-1, -1);
             while (!found_valid_move) {
                 Double bearing = random.nextDouble() * 2 * Math.PI;
@@ -99,13 +99,100 @@ public class Player implements lunch.sim.Player {
         return new Command();
     }
     
-    private Point find_boundary(Point loc, double delta_x, double delta_y) {
-        Point temp_loc = new Point(loc);
-        while (Math.abs(temp_loc.x) < 50 && Math.abs(temp_loc.y) < 50){
-            temp_loc.x += delta_x;
-            temp_loc.y += delta_y;
+    /**
+     * Helper function: takes current position and movement in one second
+     * returns point where boundary is reached
+     * @param loc
+     * @param dx
+     * @param dy
+     * @return
+    */
+    private Point find_boundary(Point loc, double dx, double dy) {
+        double curr_x = loc.x;
+        double curr_y = loc.y;
+        double t1, t2, new_x, new_y;
+        // Possible new location: (curr_x + t * dx, curr_y + t * dy)
+        if (dx > 0) {
+            if (dy > 0) {
+                // Case 1: dx > 0, dy > 0. Intersect at lower or right boundaries
+                t1 = (50.0 - curr_x) / dx;
+                t2 = (50.0 - curr_y) / dy;
+                if (t1 > t2) {
+                    // Intersect at lower boundary
+                    new_x = curr_x + t2 * dx;
+                    new_y = 50.0;
+                } else {
+                    // Intersect at right boundary
+                    new_x = 50.0;
+                    new_y = curr_y + t1 * dy;
+                }
+            } else if (dy < 0) {
+                // Case 2: dx > 0, dy < 0. Intersect at upper or right boundaries
+                t1 = (50.0 - curr_x) / dx;
+                t2 = (-50.0 - curr_y) / dy;
+                if (t1 > t2) {
+                    // Intersect at upper boundary
+                    new_x = curr_x + t2 * dx;
+                    new_y = -50.0;
+                } else {
+                    // Intersect at right boundary
+                    new_x = 50.0;
+                    new_y = curr_y + t1 * dy;
+                }
+            } else {
+                // Case 3: dx > 0, dy = 0. Intersect at right boundary
+                new_x = 50.0;
+                new_y = curr_y;
+            }
+        } else if (dx < 0) {
+            if (dy > 0) {
+                // Case 4: dx < 0, dy > 0. Intersect at lower or left boundaries
+                t1 = (-50.0 - curr_x) / dx;
+                t2 = (50.0 - curr_y) / dy;
+                if (t1 > t2) {
+                    // Intersect at lower boundary
+                    new_x = curr_x + t2 * dx;
+                    new_y = 50.0;
+                } else {
+                    // Intersect at left boundary
+                    new_x = -50.0;
+                    new_y = curr_y + t1 * dy;
+                }
+            } else if (dy < 0) {
+                // Case 5: dx < 0, dy < 0. Intersect at upper or left boundaries
+                t1 = (-50.0 - curr_x) / dx;
+                t2 = (-50.0 - curr_y) / dy;
+                if (t1 > t2) {
+                    // Intersect at upper boundary
+                    new_x = curr_x + t2 * dx;
+                    new_y = -50.0;
+                } else {
+                    // Intersect at left boundary
+                    new_x = -50.0;
+                    new_y = curr_y + t1 * dy;
+                }
+            } else {
+                // Case 3: dx < 0, dy = 0. Intersect at left boundary
+                new_x = -50.0;
+                new_y = curr_y;
+            }
+        } else {
+            if (dy > 0) {
+                // Case 7: dx = 0, dy > 0. Intersect at lower boundary
+                new_x = curr_x;
+                new_y = 50.0;
+            } else if (dy < 0) {
+                // Case 8: dx = 0, dy < 0. Intersect at upper boundary
+                new_x = curr_x;
+                new_y = -50.0;
+            } else {
+                // Case 9: dx = 0, dy = 0. No intersection (should not happen)
+                new_x = 55.0;
+                new_y = 55.0;
+            }
         }
-        return temp_loc;
+        Point new_loc = new Point(new_x, new_y);
+        return new_loc;
     }
 
     /**
@@ -135,4 +222,61 @@ public class Player implements lunch.sim.Player {
         }
         return result;
     }
+    
+    /**
+     * Helper function: move a player to a new target position
+     * returns the next position in the next second
+     * @param currPos
+     * @param targetPos
+     * @return
+     */
+    private Point moveTo(Point currPos, Point targetPos) {
+        if (Point.dist(currPos, targetPos) <= 1.0) {
+            // Distance to target position is no larget than 1
+            return targetPos;
+        }
+        double dist = Point.dist(currPos, targetPos);  // distance to target position
+        double disp_x = (targetPos.x - currPos.x) * 1.0 / dist;  // movement on the x-direction
+        double disp_y = (targetPos.y - currPos.y) * 1.0 / dist;  // movement on the y-direction
+        double next_x = currPos.x + disp_x;  // next position (x-coord)
+        double next_y = currPos.y + disp_y;  // next position (y-coord)
+        Point nextPos = new Point(next_x, next_y);
+        return nextPos;
+    }
+    
+    /**
+     * Helper function: takes current position of one animal and previous position of this animal
+     * returns movement (displacement) of this animal in one second
+     * @param animal
+     * @param prevAnimal
+     * @return
+     */
+    private Point getAnimalDisplacement(Animal animal, Animal prevAnimal) {
+        Point currPos = animal.get_location();
+        Point prevPos = prevAnimal.get_location();
+        double delta_x = curr_loc.x - prev_loc.x;
+        double delta_y = curr_loc.y - prev_loc.y;
+        Point displacement = new Point(delta_x, delta_y);
+        return displacement;
+    }
+    
+    /**
+     * Helper function: takes current position of animals and previous positions of animals
+     * returns hashmap of mapping: location in animal_arr --> movement (displacement) in one second
+     * @param animals
+     * @param prev_animals
+     * @return
+     */
+    private HashMap<Integer, Point> calculateDisplacement(ArrayList<Animal> animals, ArrayList<Animal> prev_animals) {
+        HashMap<Integer, Point> result = new HashMap<>();
+        // This assumes animal's position in array does not change
+        for (int i = 0; i < prev_animals.size(); i++){
+            Animal animal = animals.get(i);
+            Animal prevAnimal = prev_animals.get(i);
+            Point displacement = getAnimalDisplacement(animal, prevAnimal);
+            result.put(i, displacement);
+        }
+        return result;
+    }
+    
 }
