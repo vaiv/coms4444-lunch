3349542128e71060709417bdd83125b843638f94
Hollis Lehv <hlehv@aol.com>
2019-11-16 15:56:05 -0500

    Adding corner functions

--- lunch/g3/Player.java
+++ lunch/g3/Player.java
@@ -6,19 +6,27 @@ import java.util.List;
 import java.lang.Math;
 import java.util.ArrayList;
 import lunch.sim.*;
+import javafx.util.Pair; 
+
 
 public class Player implements lunch.sim.Player {
     // Initialization function.
     // members: other family members collborating with your player.
     // members_count: Number of family members.
     // t: Time limit for simulation.
+
     public void init(ArrayList<Family> members,Integer id, int f,ArrayList<Animal> animals, Integer m, Integer g, double t, Integer s) {
         return;
     };
 
     // Gets the moves from the player. Number of moves is specified by first parameter.
     public Command getCommand(ArrayList<Family> members, ArrayList<Animal> animals, PlayerState ps) {
-        return null;
+            if (!inCorner(ps)){
+                Point next_move = getNextMoveToCorner(ps);
+                return Command.createMoveCommand(next_move);
+            } else {
+                return null;
+            }
     };
 
     public boolean shouldStopEating(ArrayList<Animal> animals, PlayerState ps) {
@@ -48,22 +56,63 @@ public class Player implements lunch.sim.Player {
         return Math.sqrt(Math.pow(animal.get_location().x - ps.get_location().x, 2) + Math.pow(animal.get_location().y - ps.get_location().y, 2));
     }
 
-    public Pair locateNearestCorner(){
-        ArrayList<Pair> corners = new ArrayList<Pair>();
-		Pair top_right = new Pair(0, 100);
-		Pair top_left = new Pair(0, 0);
+    public Point locateNearestCorner(PlayerState ps){
+        ArrayList<Point> corners = new ArrayList<Point>();
+		Point top_right = new Point(50, -50);
+		Point top_left = new Point(-50, -50);
+        Point bottom_left = new Point(-50, 50);
+        Point bottom_right = new Point(50, 50);
 		corners.add(top_right);
-		return top_right;
+        corners.add(top_left);
+        corners.add(bottom_right);
+        corners.add(bottom_left);
+        Point chosenCorner = top_right;
+        double minDist = Integer.MAX_VALUE;
+        for (int i = 0; i < corners.size(); i++){
+            double dist = distanceBetweenPoints(corners.get(i), ps.get_location());
+            if (dist <= minDist){
+                minDist = dist;
+                chosenCorner = corners.get(i);
+            }
+        }
+		return chosenCorner;
     }
 
     public Point getNextMoveToCorner(PlayerState ps){
-        Double bearing = 1*2*Math.PI;
-		Point move_to_corner = new Point (ps.get_location().x + .5, ps.get_location().y + .5);
-		return move_to_corner;
+        Point nearestCorner = locateNearestCorner(ps);
+        Double bearing = getBearingFromDestination(ps.get_location(), nearestCorner);
+		Point move_to_corner = new Point(ps.get_location().x + Math.cos(bearing), ps.get_location().y + Math.sin(bearing));
+        return move_to_corner;
     }
 
-    /*
-    public Double getBearingFromDestination(){
-		Math.atan()
-	}*/
+    
+    public Double getBearingFromDestination(Point playerLocation, Point destination){
+        double deltax = destination.x - playerLocation.x;
+        double deltay = destination.y - playerLocation.y;
+		return Math.atan2(deltay, deltax);
+	}
+
+    public double distanceBetweenPoints(Point pt1, Point pt2){
+		double distance = Math.sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y));
+        return distance;
+	}
+
+    public boolean inCorner(PlayerState ps){
+        ArrayList<Point> corners = new ArrayList<Point>();
+		Point top_right = new Point(50, -50);
+		Point top_left = new Point(-50, -50);
+        Point bottom_left = new Point(-50, 50);
+        Point bottom_right = new Point(50, 50);
+		corners.add(top_right);
+        corners.add(top_left);
+        corners.add(bottom_right);
+        corners.add(bottom_left);
+        boolean corner = false;
+        for (int i = 0; i < corners.size(); i++){
+            if (distanceBetweenPoints(corners.get(i), ps.get_location()) < 3){
+                return true;
+            }
+        }
+        return corner;
+    }
 }
