76c99c1874b921459e080cf3785380ab96fe09fe
Adam Svystun <adam.svystun@gmail.com>
2019-11-16 17:29:00 -0500

    Add simple player

--- lunch/g5/Player.java
+++ lunch/g5/Player.java
@@ -1,4 +1,4 @@
-package lunch.random;
+package lunch.g5;
 
 import java.util.List;
 import java.util.Collections;
@@ -14,11 +14,21 @@ import lunch.sim.Point;
 import lunch.sim.Command;
 import lunch.sim.CommandType;
 import lunch.sim.Animal;
+import lunch.sim.AnimalType;
 import lunch.sim.Family;
 import lunch.sim.FoodType;
 import lunch.sim.PlayerState;
 
 public class Player implements lunch.sim.Player {
+
+    // Configs for calculating future matrices
+    private final int nTimesteps = 10;            // How much timesteps in the future to calculate
+    private final double dangerDistance = 5.0;   // How far way from the monkeys we still consider them in calculation
+    private final int bufferLookahead = 0;       // When calculation a matrix, how much aditional step to take
+
+    // An array to store the animals in previous turn (Mainly to know their positions, so we know where they are going)
+    private ArrayList<Animal> previousAnimals;
+
     private int seed;
     private Random random;
     private Integer id;
@@ -36,67 +46,95 @@ public class Player implements lunch.sim.Player {
         return avatars;
     }
 
-    public Command getCommand(ArrayList<Family> members, ArrayList<Animal> animals, PlayerState ps) {
 
-        Double min_dist = Double.MAX_VALUE;
-
-        for (Integer i = 0; i < animals.size(); i++) {
-            min_dist = Math.min(min_dist, Point.dist(ps.get_location(), animals.get(i).get_location()));
+    /**
+     * A function that calculates board matrices in the future.
+     *
+     * Example usage:
+     *
+     * ArrayList<Matrix> matrices = getFutureMatrices(animals);
+     * matrices.get(9).show(); // Prints a matrix at timestep 9 to System.out
+     *
+     * @param animals: An array of current elements on the board
+     * @param ps: Current player state object
+     * @return An array of matrices, where each matrix under index j is predicted state of the board after
+     *         j timesteps.
+     */
+    private ArrayList<Matrix> getFutureMatrices(ArrayList<Animal> animals, PlayerState ps) {
+        if (turn < 1) {
+            throw new RuntimeException("Cannot calculate future matrices on 0th turn");
         }
-
-        if (turn < 300) {
-            boolean found_valid_move = false;
-            Point next_move = new Point(-1, -1);
-            while (!found_valid_move) {
-                Double bearing = random.nextDouble() * 2 * Math.PI;
-                next_move = new Point(ps.get_location().x + Math.cos(bearing), ps.get_location().y + Math.sin(bearing));
-                found_valid_move = Point.within_bounds(next_move);
-            }
-            // System.out.println("move command issued");
-            turn++;
-            return Command.createMoveCommand(next_move);
+        if (previousAnimals == null) {
+            throw new RuntimeException("No previous animals saved => cannot calculate the animal directions");
         }
-
-        // abort taking out if animal is too close
-        if (min_dist < 3.0 && ps.is_player_searching() && ps.get_held_item_type() == null) {
-            // System.out.println("abort command issued");
-            // System.out.println(min_dist.toString());
-            return new Command(CommandType.ABORT);
+        ArrayList<Matrix> timesteps = new ArrayList<>();
+        // Initiate empty matrices for each timestep
+        Point ourLocation = ps.get_location();
+        for (int i = 0; i < nTimesteps; i++) {
+            int size = i + 3 + bufferLookahead * 2;
+            Matrix m = new Matrix(size, size);
+            int origin = (size - 1) / 2 + 1;
+            int originX = origin + (int)Math.round(ourLocation.x);
+            int originY = origin + (int)Math.round(ourLocation.y);
+            m.setOrigin(originX, originY);
+            timesteps.add(m);
         }
-        // keep food item back if animal is too close
-        else if (!ps.is_player_searching() && ps.get_held_item_type() != null && min_dist < 2.0) {
-            return new Command(CommandType.KEEP_BACK);
-        }
-        // move away from animal
-        else if (min_dist < 3.0) {
-            boolean found_valid_move = false;
-            Point next_move = new Point(-1, -1);
-            while (!found_valid_move) {
-                Double bearing = random.nextDouble() * 2 * Math.PI;
-                next_move = new Point(ps.get_location().x + Math.cos(bearing), ps.get_location().y + Math.sin(bearing));
-                found_valid_move = Point.within_bounds(next_move);
-            }
-            return Command.createMoveCommand(next_move);
-
-        }
-        // if no animal is near then take out food
-        else if (!ps.is_player_searching() && min_dist >= 5 && ps.get_held_item_type() == null) {
-            for (FoodType food_type : FoodType.values()) {
-                if (ps.check_availability_item(food_type)) {
-                    Command c = new Command(CommandType.TAKE_OUT, food_type);
-                    return c;
+        // Go through each animal and 'draw' it's path on the matrices
+        for (int i = 0; i < animals.size(); i++) {
+            Animal animal = animals.get(i);
+            Animal previousAnimal = previousAnimals.get(i);
+            // Only consider monkeys for now
+            if (animal.which_animal() == AnimalType.MONKEY) {
+                Point currentLocation = animal.get_location();
+                Point previousLocation = previousAnimal.get_location();
+                // Get animal direction vector
+                Point directionVector = PointUtilities.substract(currentLocation, previousLocation);
+                // We start with timestep 0, which is the current timestep
+                Point nextLocation = currentLocation;
+                for (int j = 0; j < nTimesteps; j++) {
+                    Matrix m = timesteps.get(j);
+                    // Increase all counters in the radius of dangerDistance
+                    // We look in a square of dangetDistance around a monekey;
+                    int monkeyXFrom = (int)Math.floor(nextLocation.x - dangerDistance);
+                    int monkeyXTo = (int)Math.ceil(nextLocation.x + dangerDistance);
+                    int monkeyYFrom = (int)Math.floor(nextLocation.y - dangerDistance);
+                    int monkeyYTo = (int)Math.ceil(nextLocation.y + dangerDistance);
+                    for (int x = monkeyXFrom; x <= monkeyXTo; x++) {
+                        for (int y = monkeyYFrom; y <= monkeyYTo; y++) {
+                            // If our matrix does not have these coordinates we continue
+                            if (!m.has(x, y)) {
+                                continue;
+                            }
+                            // We check if the distance is actually less than danger distance
+                            double distance = Point.dist(nextLocation, new Point(x, y));
+                            if (distance > dangerDistance) {
+                                continue;
+                            }
+                            m.increment(x, y);
+                        }
+                    }
+                    // Make a step after each timestep
+                    nextLocation = PointUtilities.add(nextLocation, directionVector);
                 }
             }
         }
-        // if no animal in vicinity then take a bite
-        else if (!ps.is_player_searching() && ps.get_held_item_type() != null) {
-            return new Command(CommandType.EAT);
-        }
-
-        // System.out.println("player is searching");
-        return new Command();
-
+        return timesteps;
     }
 
+    public Command getCommand(ArrayList<Family> members, ArrayList<Animal> animals, PlayerState ps) {
 
+        if (turn > 1) {
+            ArrayList<Matrix> matrices = getFutureMatrices(animals, ps);
+            matrices.get(9).show();
+            System.out.println("-------");
+        }
+
+        // Increase the turn counter
+        turn++;
+        // Store animals for the next turn
+        previousAnimals = animals;
+
+        return new Command();
+    }
 }
+
