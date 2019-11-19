447d99c1dc4c5e1446090719b7150fc9dacc2469
Patrick Kwok <lk2754@columbia.edu>
2019-11-18 22:17:38 -0500

    Change back avatar name

--- lunch/g6/Player.java
+++ lunch/g6/Player.java
@@ -40,7 +40,7 @@ public class Player implements lunch.sim.Player {
     
     public String init(ArrayList<Family> members, Integer id, int f, ArrayList<Animal> animals, Integer m, Integer g, double t, Integer s) {
         this.id = id;
-        avatars = "matilda";
+        avatars = "flintstone";
         random = new Random(s);
         prev_animals = new ArrayList<>();
         // Define ideal locations
