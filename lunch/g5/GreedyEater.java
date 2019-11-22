4d9cd21d445eb179414c1acacd3f20a231b48475
Ni Chang <changni@buaa.edu.cn>
2019-11-22 00:48:56 -0500

    fix bug and add cooling time

--- lunch/g5/GreedyEater.java
+++ lunch/g5/GreedyEater.java
@@ -1,12 +1,10 @@
 package lunch.g5;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import javafx.util.Pair;
 
 import lunch.sim.Animal;
-import lunch.sim.AnimalType;
 import lunch.sim.Command;
 import lunch.sim.CommandType;
 import lunch.sim.Family;
@@ -18,7 +16,7 @@ public class GreedyEater {
 	// Configs for calculating future matrices
 	private double monkeyDangerDistance = 5.0;// How far way from the monkeys we still consider them in calculation
 	private double geeseDangerDistance = 6.0; // How far way from the geese we still consider them in calculation
-	private int bufferLookahead = 0; // When calculating a matrix, how much aditional steps to consider
+	private int bufferLookahead = 0; // When calculating a matrix, how much additional steps to consider
 	private MatrixPredictor matrixPredictor;
 
 	private int clean_range = 10; // move to a place which has few animal within this range
@@ -31,7 +29,11 @@ public class GreedyEater {
 	private int towardX; // point to move to
 	private int towardY;
 	private int waitStep;
-	private FoodType searching;
+	private FoodType searching; // which food is searching
+	private FoodType[] eatingOrder;
+	private int coolingTime; // cold time after in danger
+	
+	private int turn;
 
 	public GreedyEater() {
 		super();
@@ -40,19 +42,86 @@ public class GreedyEater {
 		this.cornerY = Math.random() > 0.5 ? 1 : -1;
 		moving = false;
 		this.matrixPredictor = new MatrixPredictor(monkeyDangerDistance, geeseDangerDistance, bufferLookahead);
+		this.eatingOrder = new FoodType[] { FoodType.COOKIE, FoodType.EGG, FoodType.FRUIT1, FoodType.FRUIT2,
+				FoodType.SANDWICH1, FoodType.SANDWICH2 };
+		this.coolingTime = 0;
+		this.turn = 0;
 	}
 
-	public Command getCommand(ArrayList<Family> members, ArrayList<Animal> animals, PlayerState ps,
-			ArrayList<Animal> previousAnimals, int turn) {
+	// eating exactly at corner with a cold time
+	public Command getCommandCornerEating(ArrayList<Family> members, ArrayList<Animal> animals, PlayerState ps,
+			ArrayList<Animal> previousAnimals, int totalTurn) {
+		turn++;
+//		System.out.println(coolingTime);
+//		System.out.println(ps.get_location().x);
+//		System.out.println(ps.get_location().y);
 		// move to the corner
-		if (turn < 70) {
+		if (turn <= 70) {
+//			System.out.println("moving to corner");
+			Point next_move = moveToCorner(ps);
+			return Command.createMoveCommand(next_move);
+		}
+		if (turn == 71) {
+			// Go exactly to the corner
+			Point p = new Point(cornerX * 50, cornerY * 50);
+			return Command.createMoveCommand(p);
+		}
+
+		if(coolingTime > 0) {
+			coolingTime -= totalTurn - turn + 2;
+			coolingTime = Math.max(coolingTime, 0);
+			return new Command();
+		}
+		// danger
+		// [surrounded by monkey] or [has close goose while holding or searching
+		// sandwich]
+		if (Utilities.monkey_surround(animals, ps.get_location()) || (Utilities.goose_close(animals, ps.get_location())
+				&& (ps.get_held_item_type() == FoodType.SANDWICH || (ps.is_player_searching()
+						&& (searching == FoodType.SANDWICH1 || searching == FoodType.SANDWICH2))))) {
+			if(Utilities.monkey_surround(animals, ps.get_location()))
+				coolingTime = 35;
+			// abort if searching
+			if (ps.is_player_searching() && ps.get_held_item_type() == null) {
+//				System.out.println("in danger, stop taking out");
+				return new Command(CommandType.ABORT);
+			}
+			// put back if holding
+			if (!ps.is_player_searching() && ps.get_held_item_type() != null) {
+//				System.out.println("in danger, put back");
+				return new Command(CommandType.KEEP_BACK);
+			}
+		}
+		// safe
+		// holding something, eat it
+		if (!ps.is_player_searching() && ps.get_held_item_type() != null) {
+//			System.out.println("safe, eating");
+			return new Command(CommandType.EAT);
+		}
+		// not holding anything, take out
+		if (!ps.is_player_searching() && ps.get_held_item_type() == null) {
+			for (FoodType food_type : eatingOrder) {
+				if (ps.check_availability_item(food_type)) {
+					searching = food_type;
+					coolingTime = 0;
+					return new Command(CommandType.TAKE_OUT, food_type);
+				}
+			}
+		}
+		return new Command();
+	}
+
+	public Command getCommandUseMatrix(ArrayList<Family> members, ArrayList<Animal> animals, PlayerState ps,
+			ArrayList<Animal> previousAnimals, int totalTurn) {
+		turn++;
+		// move to the corner
+		if (turn <= 70) {
 			System.out.println("moving to corner");
 			Point next_move = moveToCorner(ps);
 			return Command.createMoveCommand(next_move);
 		}
 		int x = (int) ps.get_location().x;
 		int y = (int) ps.get_location().y;
-		if (turn == 70) {
+		if (turn == 71) {
 			// Go to an integer position in the first turn
 			Point p = new Point(x, y);
 			return Command.createMoveCommand(p);
@@ -63,7 +132,7 @@ public class GreedyEater {
 			System.out.println("finding position to move");
 			findSafePosition(animals, ps, clean_range, previousAnimals);
 		}
-		
+
 		if (waitStep > 0) {
 			waitStep--;
 			return new Command(CommandType.WAIT);
@@ -74,8 +143,9 @@ public class GreedyEater {
 			if (towardX == x && towardY == y) {
 				System.out.println("arrived, take out");
 				moving = false;
-				for (FoodType food_type : FoodType.values()) {
+				for (FoodType food_type : eatingOrder) {
 					if (ps.check_availability_item(food_type)) {
+						searching = food_type;
 						return new Command(CommandType.TAKE_OUT, food_type);
 					}
 				}
@@ -97,9 +167,9 @@ public class GreedyEater {
 		// danger
 		// [surrounded by monkey] or [has close goose while holding or searching
 		// sandwich]
-		if (Utilities.monkey_surround(animals, ps.get_location())
-				|| (Utilities.goose_close(animals, ps.get_location()) && (ps.get_held_item_type() == FoodType.SANDWICH
-						|| (ps.is_player_searching() && searching == FoodType.SANDWICH)))) {
+		if (Utilities.monkey_surround(animals, ps.get_location()) || (Utilities.goose_close(animals, ps.get_location())
+				&& (ps.get_held_item_type() == FoodType.SANDWICH || (ps.is_player_searching()
+						&& (searching == FoodType.SANDWICH1 || searching == FoodType.SANDWICH2))))) {
 			// abort if searching
 			if (ps.is_player_searching() && ps.get_held_item_type() == null) {
 				System.out.println("in danger, stop taking out");
