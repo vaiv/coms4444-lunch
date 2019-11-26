c6e73f2736d504aad56a0aaa183a2aff7294e183
vaiv <vaibhavdarbari@gmail.com>
2019-11-25 22:10:13 -0500

    food item visibility method added

--- lunch/g6/Player.java
+++ lunch/g6/Player.java
@@ -40,7 +40,7 @@ public class Player implements lunch.sim.Player {
     
     public String init(ArrayList<Family> members, Integer id, int f, ArrayList<Animal> animals, Integer m, Integer g, double t, Integer s) {
         this.id = id;
-        avatars = "guardians";
+        avatars = "flintstone";
         random = new Random(s);
         prev_animals = new ArrayList<>(animals);
         return avatars;
